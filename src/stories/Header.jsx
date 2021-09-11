ce02a0c3cc6f695a3cb31d1ebe929a7d0ce40a2e
Ian Del Duca <ian@gnar.dog>
2021-09-11 16:23:09 -0400

    update stories

--- src/stories/Header.jsx
+++ src/stories/Header.jsx
@@ -24,7 +24,7 @@ export const Header = ({ user, onLogin, onLogout, onCreateAccount }) => (
             />
           </g>
         </svg>
-        <h1>Acme</h1>
+        <h1>NETLIFY BANNER</h1>
       </div>
       <div>
         {user ? (
